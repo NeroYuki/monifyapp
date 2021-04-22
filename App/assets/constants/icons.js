324faedb27b457aabf39abdf1a5a2fd90c09cb15
Nguyen Long <18520092@gm.uit.edu.vn>
2021-04-22 21:16:28 +0700

    Create Tabbar with Icons and Labels

---
+++ App/assets/constants/icons.js
@@ -0,0 +1,13 @@
+export const overview = require('../images/overview-icon.png');
+export const budget = require('../images/budget-icon.png');
+export const profile = require('../images/profile-icon.png');
+export const tools = require('../images/tools-icon.png');
+export const plus = require('../images/plus-icon.png')
+
+export default {
+    overview,
+    budget,
+    profile,
+    tools,
+    plus
+}
---
+++ App/assets/constants/images.js
@@ -0,0 +1,6 @@
+export const plant1 = require('../images/tools-icon.png');
+
+export default {
+    plant1,
+
+};

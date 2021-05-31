9adf207b0046c7a59d1fc5f711b8010f8bf04bc2
Nguyen Long <18520092@gm.uit.edu.vn>
2021-05-31 23:32:12 +0700

    Update ChartView.js

--- App/components/OverviewScreen/ChartView.js
+++ App/components/OverviewScreen/ChartView.js
@@ -1,43 +1,53 @@
 import React, { Component } from 'react';
-import { Dimensions, Text, View } from 'react-native';
+import { Dimensions, Image, Text, View } from 'react-native';
 import { PieChart } from 'react-native-svg-charts'
+import { icons } from '../../assets/constants';
 
 export class ChartView extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            selectedIndex: 0
+            labelWidth: 0,
+            selectedIndex: "",
+            selectedAmount: "",
+            selectedIcon: "",
         }
     }
 
     render() {
 
+        // Dummy Data
         const data = [
             {
-                key: 1,
-                amount: 50,
+                key: 'Salary',
+                amount: '25',
+                icon: icons.salaryIcon,
                 svg: { fill: '#600080' },
             },
             {
-                key: 2,
-                amount: 50,
+                key: 'Food',
+                amount: '15',
+                icon: icons.foodIcon,
                 svg: { fill: '#9900cc' },
                 // arc: { outerRadius: '130%' }
             },
             {
-                key: 3,
-                amount: 40,
+                key: 'Electric Bill',
+                amount: '40',
+                icon: icons.electricBillIcon,
                 svg: { fill: '#c61aff' }
             },
             {
-                key: 4,
-                amount: 95,
+                key: 'Internet',
+                amount: '10',
+                icon: icons.educationIcon,
                 svg: { fill: '#d966ff' }
             },
             {
-                key: 5,
-                amount: 35,
+                key: 'Drug',
+                amount: '10',
+                icon: icons.investmentIcon,
                 svg: { fill: '#ecb3ff' }
             }
         ]
@@ -47,29 +57,62 @@ export class ChartView extends Component {
             return {
                 key: value.key,
                 amount: value.amount,
+                icon: value.icon,
                 svg: {
                     fill: value.svg.fill,
                     onPress: () => {
-                        this.setState({ selectedIndex: value.key })
+                        this.setState({
+                            selectedIndex: value.key,
+                            selectedAmount: (value.amount + '%'),
+                            selectedIcon: value.icon,
+                        })
                     },
                 },
                 arc: {
-                    outerRadius: (value.key === this.state.selectedIndex) ? '130%' : '100%'
+                    outerRadius: (value.key === this.state.selectedIndex) ? '120%' : '100%'
                 }
             }
         })
 
+        const deviceWidth = Dimensions.get('window').width
+
         return (
-            <View style={{ height: 300 }}>
+            <View style={{ height: 300, justifyContent: 'center' }}>
                 <PieChart
-                    style={{ height: 250, paddingTop: 50 }}
+                    style={{ height: 300 }}
                     valueAccessor={({ item }) => item.amount}
                     data={pieData}
                     spacing={0}
-                    outerRadius={'95%'}
-                >
+                    outerRadius={100}
+                />
+                <View
+                    onLayout={({ nativeEvent: { layout: { width } } }) => {
+                        this.setState({ labelWidth: width })
+                    }}
 
-                </PieChart>
+                    style={{
+                        position: 'absolute',
+                        left: deviceWidth / 2 - this.state.labelWidth / 2,
+                        alignItems: 'center'
+                    }}>
+                    <Image
+                        style={{
+                            height: 32,
+                            width: 32
+                        }}
+                        source={this.state.selectedIcon}
+                        resizeMode='contain'
+                    />
+
+                    <Text style={{
+                        paddingTop: 8,
+                        fontSize: 17,
+                        fontWeight: '500',
+                    }}>
+                        {this.state.selectedAmount}
+                    </Text>
+                    <Text>{this.state.selectedIndex}</Text>
+                </View>
             </View>
 
         )

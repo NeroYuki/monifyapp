0ef09be72e66155b3e2921bce7d6be24ec0fba70
HNLinh <hanhatlinh26@gmail.com>
2021-06-04 16:06:30 +0700

    TestCase 1.0

--- __tests__/GiaoDichCRUD.test.js
+++ __tests__/GiaoDichCRUD.test.js
@@ -71,3 +71,170 @@ test('testing delete GiaoDich', async () => {
     // console.log(caidattest)
     expect(JSON.parse(JSON.stringify(await deleteGiaoDich(GiaoDichtestupdate)))).toStrictEqual('ThanhCong')
 })
+////////////////////////////////////////////////test2///////////////////////////////////////////////////
+let idgiaodich2=new BSON.ObjectID()
+let idnguoidung2=new BSON.ObjectID()
+let idtaikhoanchuyen2=new BSON.ObjectID()
+let idtaikhoannhan2=new BSON.ObjectID()
+let loaihangmuc2=new BSON.ObjectID()
+let thoigian2= new Date('2011-04-11T10:20:30.000Z')
+
+let GiaoDich2={
+    idgiaodich:idgiaodich2,
+    idnguoidung:idnguoidung2,
+    thoigian:thoigian2,
+    idtaikhoanchuyen:idtaikhoanchuyen2,
+    idtaikhoannhan:idtaikhoannhan2,
+    sotientieudung:2000,
+    loaihangmucgd:loaihangmuc2,
+    ghichu:'Tiền chơi coin',
+}
+let GiaoDich2KT={
+    idgiaodich:idgiaodich2,
+    idnguoidung:idnguoidung2,
+    thoigian:thoigian2,
+    idtaikhoanchuyen:idtaikhoanchuyen2,
+    idtaikhoannhan:idtaikhoannhan2,
+    sotientieudung:2000,
+    sotienthunhap:null,
+    loaihangmucgd:loaihangmuc2,
+    ghichu:'Tiền chơi coin',
+}
+
+test('testing insert GiaoDich', async () => {
+    // console.log(await insertGiaoDichChuKy(GiaoDichTheoChuKy))
+    // console.log(hangmucgiaodichtest)
+   expect(JSON.parse(JSON.stringify(await insertGiaoDich(GiaoDich2)))).toStrictEqual(JSON.parse(JSON.stringify(GiaoDich2KT)))
+})
+let GiaoDichtestupdate2={
+    idgiaodich:idgiaodich2,
+    idnguoidung:idnguoidung2,
+    thoigian:new Date('2011-06-11T10:20:30.000Z'),
+    idtaikhoanchuyen:idtaikhoanchuyen2,
+    idtaikhoannhan:idtaikhoannhan2,
+    sotientieudung:6000,
+    loaihangmucgd:loaihangmuc2,
+    ghichu:'Tiền chơi đá',
+}
+let GiaoDichtestupdate2KT={
+    idgiaodich:idgiaodich2,
+    idnguoidung:idnguoidung2,
+    thoigian:new Date('2011-06-11T10:20:30.000Z'),
+    idtaikhoanchuyen:idtaikhoanchuyen2,
+    idtaikhoannhan:idtaikhoannhan2,
+    sotientieudung:6000,
+    sotienthunhap:null,
+    loaihangmucgd:loaihangmuc2,
+    ghichu:'Tiền chơi đá',
+}
+let GiaoDichtestquery2=[{
+    idgiaodich:idgiaodich2,
+    idnguoidung:idnguoidung2,
+    thoigian:new Date('2011-06-11T10:20:30.000Z'),
+    idtaikhoanchuyen:idtaikhoanchuyen2,
+    idtaikhoannhan:idtaikhoannhan2,
+    sotientieudung:6000,
+    sotienthunhap:null,
+    loaihangmucgd:loaihangmuc2,
+    ghichu:'Tiền chơi đá',
+}]
+test('testing update GiaoDich', async () => {
+    // console.log(await insertGiaoDichChuKy(GiaoDichTheoChuKy))
+    // console.log(hangmucgiaodichtest)
+   expect(JSON.parse(JSON.stringify(await updateGiaoDich(GiaoDichtestupdate2)))).toStrictEqual(JSON.parse(JSON.stringify(GiaoDichtestupdate2KT)))
+})
+test('testing querry GiaoDich', async () => {
+    //console.log(JSON.parse(JSON.stringify(await queryHangMucGiaoDich(thoigiantao=date,tenhangmuc='Sắm tết',loaihangmuc='ChiTieu',id=new BSON.ObjectID('609e4dcdec7920f2c5a218a3')))))
+    expect(JSON.parse(JSON.stringify(await queryGiaoDich({
+        idgiaodich:idgiaodich2,
+        thoigian:new Date('2011-06-11T10:20:30.000Z'),
+        sotientieudung:6000,
+        loaihangmucgd:loaihangmuc2,
+        ghichu:'Tiền chơi đá',
+    })))).toStrictEqual(JSON.parse(JSON.stringify(GiaoDichtestquery2)))
+})
+test('testing delete GiaoDich', async () => {
+    // console.log(JSON.parse(JSON.stringify(await insertCaiDat(caidattest))))
+    // console.log(caidattest)
+    expect(JSON.parse(JSON.stringify(await deleteGiaoDich(GiaoDichtestupdate2)))).toStrictEqual('ThanhCong')
+})
+////////////////////////////////////////////////////test3////////////////////////////////////////////////////////////////
+let idgiaodich3=new BSON.ObjectID()
+let idnguoidung3=new BSON.ObjectID()
+let idtaikhoanchuyen3=new BSON.ObjectID()
+let loaihangmuc3=new BSON.ObjectID()
+let thoigian3= new Date('2011-04-11T10:20:30.000Z')
+
+let GiaoDich3={
+    idgiaodich:idgiaodich3,
+    idnguoidung:idnguoidung3,
+    thoigian:thoigian3,
+    idtaikhoanchuyen:idtaikhoanchuyen3,
+    sotientieudung:2000,
+    loaihangmucgd:loaihangmuc3,
+    ghichu:'Tiền chơi coin',
+}
+let GiaoDich3KT={
+    idgiaodich:idgiaodich3,
+    idnguoidung:idnguoidung3,
+    thoigian:thoigian3,
+    idtaikhoanchuyen:idtaikhoanchuyen3,
+    idtaikhoannhan:null,
+    sotientieudung:2000,
+    sotienthunhap:null,
+    loaihangmucgd:loaihangmuc3,
+    ghichu:'Tiền chơi coin',
+}
+
+test('testing insert GiaoDich', async () => {
+    // console.log(await insertGiaoDichChuKy(GiaoDichTheoChuKy))
+    // console.log(hangmucgiaodichtest)
+   expect(JSON.parse(JSON.stringify(await insertGiaoDich(GiaoDich3)))).toStrictEqual(JSON.parse(JSON.stringify(GiaoDich3KT)))
+})
+let GiaoDichtestupdate3={
+    idgiaodich:idgiaodich3,
+    idnguoidung:idnguoidung3,
+    thoigian:new Date('2011-06-11T10:20:30.000Z'),
+    idtaikhoanchuyen:idtaikhoanchuyen3,
+    sotientieudung:6000,
+    loaihangmucgd:loaihangmuc3,
+    ghichu:'Tiền chơi đá',
+}
+let GiaoDichtestupdate3KT={
+    idgiaodich:idgiaodich3,
+    idnguoidung:idnguoidung3,
+    thoigian:new Date('2011-06-11T10:20:30.000Z'),
+    idtaikhoanchuyen:idtaikhoanchuyen3,
+    idtaikhoannhan:null,
+    sotientieudung:6000,
+    sotienthunhap:null,
+    loaihangmucgd:loaihangmuc3,
+    ghichu:'Tiền chơi đá',
+}
+let GiaoDichtestquery3=[{
+    idgiaodich:idgiaodich3,
+    idnguoidung:idnguoidung3,
+    thoigian:new Date('2011-06-11T10:20:30.000Z'),
+    idtaikhoanchuyen:idtaikhoanchuyen3,
+    idtaikhoannhan:null,
+    sotientieudung:6000,
+    sotienthunhap:null,
+    loaihangmucgd:loaihangmuc3,
+    ghichu:'Tiền chơi đá',
+}]
+test('testing update GiaoDich', async () => {
+    // console.log(await insertGiaoDichChuKy(GiaoDichTheoChuKy))
+    // console.log(hangmucgiaodichtest)
+   expect(JSON.parse(JSON.stringify(await updateGiaoDich(GiaoDichtestupdate3)))).toStrictEqual(JSON.parse(JSON.stringify(GiaoDichtestupdate3KT)))
+})
+test('testing querry GiaoDich', async () => {
+    //console.log(JSON.parse(JSON.stringify(await queryHangMucGiaoDich(thoigiantao=date,tenhangmuc='Sắm tết',loaihangmuc='ChiTieu',id=new BSON.ObjectID('609e4dcdec7920f2c5a218a3')))))
+    expect(JSON.parse(JSON.stringify(await queryGiaoDich({
+        idgiaodich:idgiaodich3,
+    })))).toStrictEqual(JSON.parse(JSON.stringify(GiaoDichtestquery3)))
+})
+test('testing delete GiaoDich', async () => {
+    // console.log(JSON.parse(JSON.stringify(await insertCaiDat(caidattest))))
+    // console.log(caidattest)
+    expect(JSON.parse(JSON.stringify(await deleteGiaoDich(GiaoDichtestupdate3)))).toStrictEqual('ThanhCong')
+})
--- __tests__/GiaoDichChuKyCRUD.test.js
+++ __tests__/GiaoDichChuKyCRUD.test.js
@@ -79,3 +79,182 @@ test('testing delete GiaoDichTheoChuKy', async () => {
     // console.log(caidattest)
     expect(JSON.parse(JSON.stringify(await deleteGiaoDichChuKy(GiaoDichTheoChuKytestupdate)))).toStrictEqual('ThanhCong')
 })
+//////////////////////////////////////test2/////////////////////////////////////////////////////////////////////
+let idgiaodichtheochuky2=new BSON.ObjectID()
+let idnguoidung2=new BSON.ObjectID()
+let idtaikhoanchuyen2=new BSON.ObjectID()
+let idtaikhoannhan2=new BSON.ObjectID()
+let loaihangmuc2=new BSON.ObjectID()
+let thoigian2= new Date('2011-04-11T10:20:30.000Z')
+
+let GiaoDichTheoChuKy2={
+    idgiaodichtheochuky:idgiaodichtheochuky2,
+    idnguoidung:idnguoidung2,
+    thoigian:thoigian2,
+    idtaikhoanchuyen:idtaikhoanchuyen2,
+    idtaikhoannhan:idtaikhoannhan2,
+    sotienthunhap:5000,
+    loaihangmucgd:loaihangmuc2,
+    chukygiaodichtheongay:60,
+    ghichu:'Tiền chơi coin',
+}
+let GiaoDichTheoChuKy2KT={
+    idgiaodichtheochuky:idgiaodichtheochuky2,
+    idnguoidung:idnguoidung2,
+    thoigian:thoigian2,
+    idtaikhoanchuyen:idtaikhoanchuyen2,
+    idtaikhoannhan:idtaikhoannhan2,
+    sotientieudung:null,
+    sotienthunhap:5000,
+    loaihangmucgd:loaihangmuc2,
+    chukygiaodichtheongay:60,
+    chukygiaodichtheothang:null,
+    ghichu:'Tiền chơi coin',
+}
+
+test('testing insert GiaoDichTheoChuKy', async () => {
+    // console.log(await insertGiaoDichChuKy(GiaoDichTheoChuKy))
+    // console.log(hangmucgiaodichtest)
+   expect(JSON.parse(JSON.stringify(await insertGiaoDichChuKy(GiaoDichTheoChuKy2)))).toStrictEqual(JSON.parse(JSON.stringify(GiaoDichTheoChuKy2KT)))
+})
+let GiaoDichTheoChuKytestupdate2={
+    idgiaodichtheochuky:idgiaodichtheochuky2,
+    idnguoidung:idnguoidung2,
+    thoigian:new Date('2011-06-11T10:20:30.000Z'),
+    idtaikhoanchuyen:idtaikhoanchuyen2,
+    idtaikhoannhan:idtaikhoannhan2,
+    sotienthunhap:15000,
+    loaihangmucgd:loaihangmuc2,
+    chukygiaodichtheongay:120,
+    ghichu:'Tiền chơi đá',
+}
+let GiaoDichTheoChuKytestupdate2KT={
+    idgiaodichtheochuky:idgiaodichtheochuky2,
+    idnguoidung:idnguoidung2,
+    thoigian:new Date('2011-06-11T10:20:30.000Z'),
+    idtaikhoanchuyen:idtaikhoanchuyen2,
+    idtaikhoannhan:idtaikhoannhan2,
+    sotientieudung:null,
+    sotienthunhap:15000,
+    loaihangmucgd:loaihangmuc2,
+    chukygiaodichtheongay:120,
+    chukygiaodichtheothang:null,
+    ghichu:'Tiền chơi đá',
+}
+let GiaoDichTheoChuKytestquery2=[{
+    idgiaodichtheochuky:idgiaodichtheochuky2,
+    idnguoidung:idnguoidung2,
+    thoigian:new Date('2011-06-11T10:20:30.000Z'),
+    idtaikhoanchuyen:idtaikhoanchuyen2,
+    idtaikhoannhan:idtaikhoannhan2,
+    sotientieudung:null,
+    sotienthunhap:15000,
+    loaihangmucgd:loaihangmuc2,
+    chukygiaodichtheongay:120,
+    chukygiaodichtheothang:null,
+    ghichu:'Tiền chơi đá',
+}]
+test('testing update GiaoDichTheoChuKy', async () => {
+    // console.log(await insertGiaoDichChuKy(GiaoDichTheoChuKy))
+    // console.log(hangmucgiaodichtest)
+   expect(JSON.parse(JSON.stringify(await updateGiaoDichChuKy(GiaoDichTheoChuKytestupdate2)))).toStrictEqual(JSON.parse(JSON.stringify(GiaoDichTheoChuKytestupdate2KT)))
+})
+test('testing querry GiaoDichTheoChuKy', async () => {
+    //console.log(JSON.parse(JSON.stringify(await queryHangMucGiaoDich(thoigiantao=date,tenhangmuc='Sắm tết',loaihangmuc='ChiTieu',id=new BSON.ObjectID('609e4dcdec7920f2c5a218a3')))))
+    expect(JSON.parse(JSON.stringify(await queryGiaoDichChuKy({
+        thoigian:new Date('2011-06-11T10:20:30.000Z'),
+        idtaikhoanchuyen:idtaikhoanchuyen2,
+        idtaikhoannhan:idtaikhoannhan2,
+        sotienthunhap:15000,
+        loaihangmucgd:loaihangmuc2,
+    })))).toStrictEqual(JSON.parse(JSON.stringify(GiaoDichTheoChuKytestquery2)))
+})
+test('testing delete GiaoDichTheoChuKy', async () => {
+    // console.log(JSON.parse(JSON.stringify(await insertCaiDat(caidattest))))
+    // console.log(caidattest)
+    expect(JSON.parse(JSON.stringify(await deleteGiaoDichChuKy(GiaoDichTheoChuKytestupdate2)))).toStrictEqual('ThanhCong')
+})
+////////////////////////////////////////test3//////////////////////////////////////////////////////////////////////
+let idgiaodichtheochuky3=new BSON.ObjectID()
+let idnguoidung3=new BSON.ObjectID()
+let loaihangmuc3=new BSON.ObjectID()
+let thoigian3= new Date('2011-04-11T10:20:30.000Z')
+
+let GiaoDichTheoChuKy3={
+    idgiaodichtheochuky:idgiaodichtheochuky3,
+    idnguoidung:idnguoidung3,
+    thoigian:thoigian3,
+    loaihangmucgd:loaihangmuc3,
+    chukygiaodichtheongay:60,
+    ghichu:'Tiền chơi coin',
+}
+let GiaoDichTheoChuKy3KT={
+    idgiaodichtheochuky:idgiaodichtheochuky3,
+    idnguoidung:idnguoidung3,
+    thoigian:thoigian3,
+    idtaikhoanchuyen:null,
+    idtaikhoannhan:null,
+    sotientieudung:null,
+    sotienthunhap:null,
+    loaihangmucgd:loaihangmuc3,
+    chukygiaodichtheongay:60,
+    chukygiaodichtheothang:null,
+    ghichu:'Tiền chơi coin',
+}
+
+test('testing insert GiaoDichTheoChuKy', async () => {
+    // console.log(await insertGiaoDichChuKy(GiaoDichTheoChuKy))
+    // console.log(hangmucgiaodichtest)
+   expect(JSON.parse(JSON.stringify(await insertGiaoDichChuKy(GiaoDichTheoChuKy3)))).toStrictEqual(JSON.parse(JSON.stringify(GiaoDichTheoChuKy3KT)))
+})
+let GiaoDichTheoChuKytestupdate3={
+    idgiaodichtheochuky:idgiaodichtheochuky3,
+    idnguoidung:idnguoidung3,
+    thoigian:new Date('2011-06-11T10:20:30.000Z'),
+    loaihangmucgd:loaihangmuc3,
+    chukygiaodichtheongay:120,
+    ghichu:'Tiền chơi đá',
+}
+let GiaoDichTheoChuKytestupdate3KT={
+    idgiaodichtheochuky:idgiaodichtheochuky3,
+    idnguoidung:idnguoidung3,
+    thoigian:new Date('2011-06-11T10:20:30.000Z'),
+    idtaikhoanchuyen:null,
+    idtaikhoannhan:null,
+    sotientieudung:null,
+    sotienthunhap:null,
+    loaihangmucgd:loaihangmuc3,
+    chukygiaodichtheongay:120,
+    chukygiaodichtheothang:null,
+    ghichu:'Tiền chơi đá',
+}
+let GiaoDichTheoChuKytestquery3=[{
+    idgiaodichtheochuky:idgiaodichtheochuky3,
+    idnguoidung:idnguoidung3,
+    thoigian:new Date('2011-06-11T10:20:30.000Z'),
+    idtaikhoanchuyen:null,
+    idtaikhoannhan:null,
+    sotientieudung:null,
+    sotienthunhap:null,
+    loaihangmucgd:loaihangmuc3,
+    chukygiaodichtheongay:120,
+    chukygiaodichtheothang:null,
+    ghichu:'Tiền chơi đá',
+}]
+test('testing update GiaoDichTheoChuKy', async () => {
+    // console.log(await insertGiaoDichChuKy(GiaoDichTheoChuKy))
+    // console.log(hangmucgiaodichtest)
+   expect(JSON.parse(JSON.stringify(await updateGiaoDichChuKy(GiaoDichTheoChuKytestupdate3)))).toStrictEqual(JSON.parse(JSON.stringify(GiaoDichTheoChuKytestupdate3KT)))
+})
+test('testing querry GiaoDichTheoChuKy', async () => {
+    //console.log(JSON.parse(JSON.stringify(await queryHangMucGiaoDich(thoigiantao=date,tenhangmuc='Sắm tết',loaihangmuc='ChiTieu',id=new BSON.ObjectID('609e4dcdec7920f2c5a218a3')))))
+    expect(JSON.parse(JSON.stringify(await queryGiaoDichChuKy({
+        thoigian:new Date('2011-06-11T10:20:30.000Z'),
+        loaihangmucgd:loaihangmuc3,
+    })))).toStrictEqual(JSON.parse(JSON.stringify(GiaoDichTheoChuKytestquery3)))
+})
+test('testing delete GiaoDichTheoChuKy', async () => {
+    // console.log(JSON.parse(JSON.stringify(await insertCaiDat(caidattest))))
+    // console.log(caidattest)
+    expect(JSON.parse(JSON.stringify(await deleteGiaoDichChuKy(GiaoDichTheoChuKytestupdate3)))).toStrictEqual('ThanhCong')
+})

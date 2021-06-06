1045883083fedd903888ca745d147be4c825aadb
ahahavip <55023509+ahahavip@users.noreply.github.com>
2021-06-06 18:30:55 +0700

    aaa

--- App/components/index.js
+++ App/components/index.js
@@ -1,21 +1,55 @@
 import { TestText } from './TestText'
 import { TestButton } from './TestButton'
+import { BudgetHeader } from './BudgetScreen/BudgetHeader'
 import { TransactionEditor } from './TransactionEditor'
 import { WalletHeader } from './WalletHeader'
 import { TabSwitcher } from './TabSwitcher'
 import { CategoriesModal } from './CategoriesModal'
-//import { TimespanDefaultFragment } from './TimespanPicker/TimespanDefaultFragment'
+import { TimespanDefaultFragment } from './TimespanPicker/TimespanDefaultFragment'
 import { CustomTimespanFragment } from './TimespanPicker/CustomTimespanFragment'
 import { TimespanPicker } from './TimespanPicker' 
-
+import { GenericIconList } from './GenericIconList'
+import { DescriptiveButton } from './DescriptiveButton'
+import { CreateModal } from './CreateModal'
+import { CategoryEditor } from './CategoryEditor'
+import { GenericSettingField } from './GenericSettingField'
+import { GenericSelectionModal } from './GenericSelectionModal'
+import { WalletEntry } from './WalletManager/WalletEntry'
+import { WalletSearchModal } from './WalletManager/WalletSearchModal' 
+import { LoanEntry } from './LoanManager/LoanEntry'
+import { LoanSearchModal } from './LoanManager/LoanSearchModal'
+import { SavingEntry } from './SavingManager/SavingEntry'
+import { SavingSearchModal } from './SavingManager/SavingSearchModal'
+import { RecurringBillEntry } from './RecurringBillManager/RecurringBillEntry' 
+import { RecurringBillSearchModal } from './RecurringBillManager/RecurringBillSearchModal'
+import { GenericInputModal } from './GenericInputModal'
+import { ColorPickerModal } from './ColorPickerModal'
+ 
 export {
     TabSwitcher,
     WalletHeader,
     TestButton,
     TestText,
+    BudgetHeader,
     TransactionEditor,
     CategoriesModal,
-    //TimespanDefaultFragment,
+    TimespanDefaultFragment,
     CustomTimespanFragment,
     TimespanPicker,
+    GenericIconList,
+    DescriptiveButton,
+    CreateModal,
+    CategoryEditor,
+    GenericSettingField,
+    GenericSelectionModal,
+    WalletEntry,
+    WalletSearchModal,
+    LoanEntry,
+    LoanSearchModal,
+    SavingEntry,
+    SavingSearchModal,
+    RecurringBillEntry,
+    RecurringBillSearchModal,
+    GenericInputModal,
+    ColorPickerModal,
 }
